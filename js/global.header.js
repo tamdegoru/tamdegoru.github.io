982c555d3475c235391cff1ac2650f0d9cc22907
Shinigamicatx <shinigami.neeeko@gmail.com>
2026-02-23 00:26:47 +0200

    Add scroll event listener to header for styling

---
+++ js/global.header.js
@@ -0,0 +1,11 @@
+<script>
+    const header = document.querySelector(".header");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 80) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+    });
+</script>
