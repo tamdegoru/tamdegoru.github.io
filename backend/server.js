58c3fb0d98582a929acb9dc32be49d035f968ee1
tamdegoru <firexlion@gmail.com>
2026-03-03 19:39:22 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,7 @@ app.get("/health", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Listening on", PORT));
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
